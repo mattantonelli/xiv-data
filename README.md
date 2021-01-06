c4162ed606fac759c0c14ed8283f951bd97206e2
Matt Antonelli <contact@raelys.com>
2021-01-06 17:28:35 -0500

    Update extract script for FFXIV Collect data
    Also updates README with instructions on how to handle images.

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ This is a collection of CSV and image files used for updating my Final Fantasy X
 4. Edit the paths in the script as necessary
 5. Run the script and check the changes into GitHub (Note: Windows has a serious fear of bats)
 
+### Images
+
+Image data is no longer stored in this repository. The extract script will generate a `ui.zip` file in this directory, but it is up to the user to unarchive this where it is needed. My applications will expect the images to exist under `/var/rails/images/ffxiv`. After creating this directory with the necessary permissions, you can use the following script to unzip the archive in this directory:
+
+```sh
+#!/usr/bin/env sh
+mv ui.zip /var/rails/images/ffxiv
+cd /var/rails/images/ffxiv
+unzip -oq ui.zip
+rm ui.zip
+```
+
 ---
 
 FINAL FANTASY is a registered trademark of Square Enix Holdings Co., Ltd.
--- extract.bat
+++ extract.bat
@@ -6,11 +6,22 @@ SET /p VERSION=<%GAMEPATH%\game\ffxivgame.ver
 ECHO Setting SC definition to the latest game version...
 COPY %GAMEPATH%\game\ffxivgame.ver Definitions\game.ver
 
-ECHO Extracting game data...
-.\SaintCoinach.Cmd.exe %GAMEPATH% "allexd ENpcBase ENpcResident TripleTriad TripleTriadCard TripleTriadCardResident TripleTriadCardType TripleTriadRule PlaceName Quest ContentFinderCondition Achievement"
-.\SaintCoinach.Cmd.exe %GAMEPATH% "rawexd Level TripleTriad Map"
-.\SaintCoinach.Cmd.exe %GAMEPATH% "ui 082101 082999"
+ECHO [%TIME%] Extracting game data...
+.\SaintCoinach.Cmd.exe %GAMEPATH% "allexd Achievement AchievementCategory AchievementKind Action AozActionTransient BuddyEquip CabinetCategory Companion CompanionMove CompanionTransient ContentFinderCondition Emote EmoteCategory ENpcBase ENpcResident Item MinionRace MinionSkillType Mount MountTransient Orchestrion OrchestrionCategory PlaceName Quest Recipe TextCommand TripleTriad TripleTriadCard TripleTriadCardResident TripleTriadCardType TripleTriadRule"
+.\SaintCoinach.Cmd.exe %GAMEPATH% "rawexd Level TripleTriad Map Achievement Cabinet ItemAction CharaMakeCustomize AozAction"
 
-ECHO Copying game data to the local repository...
-XCOPY /S /Y /Q %CD%\%VERSION% %REPOPATH%
+ECHO [%TIME%] Extracting images...
+.\SaintCoinach.Cmd.exe %GAMEPATH% "ui 000000 069999"
+.\SaintCoinach.Cmd.exe %GAMEPATH% "ui 082100 082999"
+for /d %%i in (%CD%\%VERSION%\ui\icon\*) do (cd "%%i" & rmdir /S /Q hq 2>NUL)
+
+ECHO [%TIME%] Compressing images...
+powershell Compress-Archive -Force %CD%\%VERSION%\ui %CD%\%VERSION%\ui.zip
+
+ECHO [%TIME%] Copying game data to the local repository...
+XCOPY /S /Y /Q %CD%\%VERSION%\exd-all %REPOPATH%\exd-all
+XCOPY /S /Y /Q %CD%\%VERSION%\rawexd %REPOPATH%\rawexd
+MOVE /Y %CD%\%VERSION%\ui.zip %REPOPATH%
+
+ECHO [%TIME%] Extract complete.
 pause
