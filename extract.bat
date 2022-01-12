81fa194c494cede38f6ec59b35280a0677dc7546
Matt Antonelli <contact@raelys.com>
2022-01-11 19:55:31 -0500

    Add InstanceContent for instance order

--- extract.bat
+++ extract.bat
@@ -11,7 +11,7 @@ COPY %GAMEPATH%\game\ffxivgame.ver Definitions\game.ver
 
 ECHO [%TIME%] Extracting game data...
 .\SaintCoinach.Cmd.exe %GAMEPATH% "allexd Achievement AchievementCategory AchievementKind Action ActionTransient Addon AozActionTransient BuddyEquip Companion CompanionMove CompanionTransient ContentFinderCondition Emote EmoteCategory ENpcBase ENpcResident Item MinionRace MinionSkillType Mount MountTransient MYCWarResultNotebook Orchestrion OrchestrionCategory Ornament PlaceName Quest SpecialShop TextCommand Title TripleTriad TripleTriadCard TripleTriadCardResident TripleTriadCardType TripleTriadRule"
-.\SaintCoinach.Cmd.exe %GAMEPATH% "rawexd Achievement AozAction Cabinet CabinetCategory CharaMakeCustomize Emote GCScripShopItem GilShopItem ItemAction Level Map OrchestrionPath OrchestrionUiParam Recipe SpecialShop TripleTriad"
+.\SaintCoinach.Cmd.exe %GAMEPATH% "rawexd Achievement AozAction Cabinet CabinetCategory CharaMakeCustomize Emote GCScripShopItem GilShopItem InstanceContent ItemAction Level Map OrchestrionPath OrchestrionUiParam Recipe SpecialShop TripleTriad"
 
 ECHO [%TIME%] Extracting images...
 .\SaintCoinach.Cmd.exe %GAMEPATH% "ui 000000 069999"
