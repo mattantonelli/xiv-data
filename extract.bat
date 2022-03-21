96983dbe3c6ef91917302bfe26c5dcb77d8233b8
Matt Antonelli <contact@raelys.com>
2022-03-21 10:44:06 -0400

    Fix extract scripts
    Fixes the working directory getting lost while cleaning up images. DRYs
    things up for good measure. Adds face paint icon range.

--- extract.bat
+++ extract.bat
@@ -5,6 +5,7 @@ SET GAMEPATH="C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY XIV On
 SET REPOPATH="C:\Users\Matt\Documents\Code\xiv-data"
 SET FFMPEG="C:\Users\Matt\Documents\Code\xiv-data\ffmpeg.exe"
 SET /p VERSION=<%GAMEPATH%\game\ffxivgame.ver
+SET DATAPATH="%CD%\%VERSION%"
 
 ECHO Setting SC definition to the latest game version...
 COPY %GAMEPATH%\game\ffxivgame.ver Definitions\game.ver
@@ -17,30 +18,31 @@ ECHO [%TIME%] Extracting images...
 .\SaintCoinach.Cmd.exe %GAMEPATH% "ui 000000 069999"
 .\SaintCoinach.Cmd.exe %GAMEPATH% "ui 072200 072652"
 .\SaintCoinach.Cmd.exe %GAMEPATH% "ui 082100 082999"
-.\SaintCoinach.Cmd.exe %GAMEPATH% "uihd 131000 138999"
-for /d %%i in (%CD%\%VERSION%\ui\icon\*) do (cd "%%i" & rmdir /S /Q hq 2>NUL)
+.\SaintCoinach.Cmd.exe %GAMEPATH% "uihd 130000 138999"
+for /d %%i in (%DATAPATH%\ui\icon\*) do (cd "%%i" & rmdir /S /Q hq 2>NUL)
+CD "%DATAPATH%\.."
 
 ECHO [%TIME%] Compressing images...
-"C:\Program Files\7-Zip\7z.exe" a %CD%\%VERSION%\ui.zip %CD%\%VERSION%\ui\* > NUL
+"C:\Program Files\7-Zip\7z.exe" a %DATAPATH%\ui.zip %DATAPATH%\ui\* > NUL
 
 ECHO Processing music...
 .\SaintCoinach.Cmd.exe %GAMEPATH% "bgm bgm_ride bgm_orch"
-for %%f in (%CD%\%VERSION%\music\ffxiv\*.ogg) do %FFMPEG% -y -i "%%f" -t 00:00:15.0 -b:a 32k -ar 22050 -loglevel quiet "%CD%\%VERSION%\music\%%~nf.ogg"
-for %%f in (%CD%\%VERSION%\music\ffxiv\Orchestrion\*.ogg) do (
+for %%f in (%DATAPATH%\music\ffxiv\*.ogg) do %FFMPEG% -y -i "%%f" -t 00:00:15.0 -b:a 32k -ar 22050 -loglevel quiet "%DATAPATH%\music\%%~nf.ogg"
+for %%f in (%DATAPATH%\music\ffxiv\Orchestrion\*.ogg) do (
   set name=%%~nf
   set name=!name:~0,12!
-  %FFMPEG% -y -i "%%f" -ss 00:00:10.0 -t 00:00:15.0 -b:a 32k -ar 22050 -loglevel quiet "%CD%\%VERSION%\music\!name!.ogg"
+  %FFMPEG% -y -i "%%f" -ss 00:00:10.0 -t 00:00:15.0 -b:a 32k -ar 22050 -loglevel quiet "%DATAPATH%\music\!name!.ogg"
 )
-rmdir /S /Q %CD%\%VERSION%\music\ffxiv
+rmdir /S /Q %DATAPATH%\music\ffxiv
 
 ECHO [%TIME%] Compressing music...
-"C:\Program Files\7-Zip\7z.exe" a %CD%\%VERSION%\music.zip %CD%\%VERSION%\music\* > NUL
+"C:\Program Files\7-Zip\7z.exe" a %DATAPATH%\music.zip %DATAPATH%\music\* > NUL
 
 ECHO [%TIME%] Copying game data to the local repository...
-XCOPY /S /Y /Q %CD%\%VERSION%\exd-all %REPOPATH%\exd-all
-XCOPY /S /Y /Q %CD%\%VERSION%\rawexd %REPOPATH%\rawexd
-MOVE /Y %CD%\%VERSION%\ui.zip %REPOPATH%
-MOVE /Y %CD%\%VERSION%\music.zip %REPOPATH%
+XCOPY /S /Y /Q %DATAPATH%\exd-all %REPOPATH%\exd-all
+XCOPY /S /Y /Q %DATAPATH%\rawexd %REPOPATH%\rawexd
+MOVE /Y %DATAPATH%\ui.zip %REPOPATH%
+MOVE /Y %DATAPATH%\music.zip %REPOPATH%
 
 ECHO [%TIME%] Extract complete.
 pause
--- extract_triad_only.bat
+++ extract_triad_only.bat
@@ -3,6 +3,7 @@
 SET GAMEPATH="C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY XIV Online"
 SET REPOPATH="C:\Users\Matt\Documents\Code\xiv-data"
 SET /p VERSION=<%GAMEPATH%\game\ffxivgame.ver
+SET DATAPATH="%CD%\%VERSION%"
 
 ECHO Setting SC definition to the latest game version...
 COPY %GAMEPATH%\game\ffxivgame.ver Definitions\game.ver
@@ -13,12 +14,12 @@ ECHO [%TIME%] Extracting game data...
 .\SaintCoinach.Cmd.exe %GAMEPATH% "ui 082100 082999"
 
 ECHO [%TIME%] Compressing images...
-"C:\Program Files\7-Zip\7z.exe" a %CD%\%VERSION%\ui.zip %CD%\%VERSION%\ui\* > NUL
+"C:\Program Files\7-Zip\7z.exe" a %DATAPATH%\ui.zip %DATAPATH%\ui\* > NUL
 
 ECHO [%TIME%] Copying game data to the local repository...
-XCOPY /S /Y /Q %CD%\%VERSION%\exd-all %REPOPATH%\exd-all
-XCOPY /S /Y /Q %CD%\%VERSION%\rawexd %REPOPATH%\rawexd
-MOVE /Y %CD%\%VERSION%\ui.zip %REPOPATH%
+XCOPY /S /Y /Q %DATAPATH%\exd-all %REPOPATH%\exd-all
+XCOPY /S /Y /Q %DATAPATH%\rawexd %REPOPATH%\rawexd
+MOVE /Y %DATAPATH%\ui.zip %REPOPATH%
 
 ECHO [%TIME%] Extract complete.
 pause
